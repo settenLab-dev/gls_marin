79ba209a322cb01cb2b9a609ad3ce20c38f863ee
yoshimura <yoshimura@settenlab.jp>
2017-06-01 19:35:54 +0900

    GLS_MARIN-12 ユーザーログイン
    ログアウト時にPOSTされてしまうため
    GETで必要な値を一緒に送信するようセット

--- includes/box/common/header_common.php
+++ includes/box/common/header_common.php
@@ -3,18 +3,18 @@
 
 <header id="header2016">
 	<div id="header-inner" class="clearfix">
-        <div class="left">
-            <h1 class="logo"><a href="/"><img src="images/index2016/logo.png" alt="PlayBooking"></a></h1>
-        </div>
-        <div class="left">
+		<div class="left">
+				<h1 class="logo"><a href="/"><img src="images/index2016/logo.png" alt="PlayBooking"></a></h1>
+		</div>
+		<div class="left">
 			<form name="key-search" method="POST" action="/plan-search.html">
 			<div class="key-search-box">
-	  			<input type="search" name="free" placeholder="例）フライボード、ダイビング" value="" class="search-inner">
-	  			<!--<input type="hidden" name="undecide_sch" value="1">-->
+					<input type="search" name="free" placeholder="例）フライボード、ダイビング" value="" class="search-inner">
+					<!--<input type="hidden" name="undecide_sch" value="1">-->
 				<button class="key-search-btn" onclick="document.key-search.submit();">検索</button>
 			</div>
 			</form>
-        </div>
+		</div>
 
 		<!--	<ul class="header_keyword">
 				<li>【話題のワード】</li>
@@ -23,43 +23,39 @@
 			</ul>
 		-->
 
-	<div class="right">
-	    	<aside class="header_nav clearfix">
-
-
-
-
-   <?php if (!$sess->sessionCheck()) { ?>
-		<div class="mybox" style="line-height: 8px;margin-top: 15px;">
-			<a href="/login.html">
-			<img src="images/common/icon_login.png" style="float: left; margin:0 5px; width:25px;">
-			<div>
-			<!--ようこそ！<span>ゲストさん</span><br>-->
-			<br>会員登録/ログイン
-			</div>
-			</a>
-		</div>
-
-   <?php }else{ ?>
-
-		<div class="mybox">
-			<div>
-			<img src="images/common/icon_login.png" style="float: left; margin: 5px;width:25px;">
-			</div>
-			<div>
-			ようこそ！<?php print $sess->getSessionByKey($sess->getSessionLogninKey(), "MEMBER_HANDLENAME")?>さん</span><br>
-			（<a href="/mypage.html">マイページへ</a>／<a href="" onClick="document.logout.submit();return false">ログアウト</a>）
-			</div>
+		<div class="right">
+			<aside class="header_nav clearfix">
+				<?php if (!$sess->sessionCheck()) { ?>
+					<div class="mybox" style="line-height: 8px;margin-top: 15px;">
+						<a href="/login.html">
+							<img src="images/common/icon_login.png" style="float: left; margin:0 5px; width:25px;">
+							<div>
+								<!--ようこそ！<span>ゲストさん</span><br>-->
+								<br>会員登録/ログイン
+							</div>
+						</a>
+					</div>
+				<?php }else{ ?>
+					<div class="mybox">
+						<div>
+							<img src="images/common/icon_login.png" style="float: left; margin: 5px;width:25px;">
+						</div>
+						<div>
+							ようこそ！<?php print $sess->getSessionByKey($sess->getSessionLogninKey(), "MEMBER_HANDLENAME")?>さん</span><br>
+							（<a href="/mypage.html">マイページへ</a>／<a href="" onClick="document.logout.submit();return false">ログアウト</a>）
+						</div>
 						<form action="<?php print $_SERVER['REQUEST_URI']?>" method="post" name="logout">
 							<input type="hidden" name="logout" value="ログアウト">
-							
-					       	</form>
-
+							<?php if(!empty($_GET["cid"])):?>
+								<input type="hidden" name="COMPANY_ID" value="<?php echo $_GET["cid"];?>">
+							<?php endif;?>
+							<?php if(!empty($_GET["pid"])):?>
+								<input type="hidden" name="SHOPPLAN_ID" value="<?php echo $_GET["pid"];?>">
+							<?php endif;?>
+						</form>
+					</div>
+				<?php } ?>
+			</aside>
 		</div>
-
-   <?php } ?>
-
-	        </aside>
-		</div>
-    </div>
+	</div>
 </header>
